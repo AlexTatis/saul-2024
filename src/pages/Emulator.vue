5f4795758a37401d4ccf97d56118bfa9bc204bb5
AlexTatis <alejandrocampoh@gmail.com>
2024-06-19 18:07:43 +0200

    Added emulator to /public directory

--- src/pages/Emulator.vue
+++ src/pages/Emulator.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+</script>
+
 <template>
-    <iframe class="block h-screen w-screen" src="/src/emulator/index.html" frameborder="0"></iframe>
+    <iframe class="block h-screen w-screen" src="/saul-2024/emulator/index.html" frameborder="0"></iframe>
 </template>
